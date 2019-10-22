a6247b666b5d3310ed310420e41e71cc374b8111
ebender17 <36086595+ebender17@users.noreply.github.com>
2019-10-22 16:06:03 -0400

    Size class

---
+++ src/Size.java
@@ -0,0 +1,36 @@
+public class Size implements Chargable{
+    String name;
+    double radius;
+    double price;
+
+    public Size(String name, double radius, double price) {
+        this.name = name;
+        this.radius = radius;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public double getPrice() {
+        return 0;
+    }
+}
