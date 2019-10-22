a734627bb7d1e2572dc1460613732917e9ad4a5d
ebender17 <36086595+ebender17@users.noreply.github.com>
2019-10-22 16:06:16 -0400

    Crust class

---
+++ src/Crust.java
@@ -0,0 +1,26 @@
+public class Crust implements Chargable {
+    String name;
+    double price;
+
+    public Crust(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public double getPrice() {
+        return 0;
+    }
+}
