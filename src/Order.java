4f1afddd11938e4207cc10cf0d706c5e512d903c
ebender17 <36086595+ebender17@users.noreply.github.com>
2019-10-23 13:37:21 -0400

    completed getRecipt class

--- src/Order.java
+++ src/Order.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 
 public class Order {
     ArrayList<Pizza> pizzas;
+    double TAX_RATE = 0.07;
 
     public Order() {
         this.pizzas = new ArrayList<Pizza>();
@@ -16,7 +17,33 @@ public class Order {
     }
 
     public String getRecipt() {
-        return "";
+        String receiptString =
+               "\t      ____       _                             \n"
+              +"\t    /  __  \\     (_) ___    __     ____     \n"
+              +"\t   /  /_/  /     / / /_   /  /_    /   /  _  /   \n"
+              +"\t  /  ___/     / /   /  /_    /  /_  / /_/ /    \n"
+              +"\t /_/          /_/  / ___/  / ___/ \\___/  \n";
+        receiptString += "===============================================\n";
+        //Crust and Size
+        for (int pizzaCounter = 0; pizzaCounter < pizzas.size(); pizzaCounter++) {
+            receiptString += "------ Pizza" + (pizzaCounter + 1) + " : " + pizzas.get(pizzaCounter).getTotal() + " -----\n";
+            receiptString+= String.format("%-10s & %-10s\t\t$%5.2f\n",
+                    pizzas.get(pizzaCounter).getCrust().getName(),
+                    pizzas.get(pizzaCounter).getSize().getName(),
+                    pizzas.get(pizzaCounter).getSize().getPrice()
+            );
+            //Toppings
+            for (Ingredient ingredient: this.pizzas.get(pizzaCounter).getIngredients()) {
+                receiptString += String.format("    %-20s\t\t$%5.2f\n", ingredient.getName(), ingredient.getPrice());
+            }
+
+            receiptString += "\n\n";
+            receiptString += String.format("Subtotal\t\t\t$%5.2f\n", this.getTotal());
+            receiptString += String.format("Tax\t\t\t$%5.2f\n", this.getTotal() * TAX_RATE);
+            receiptString += "---------------------------------------------------------------------\n";
+            receiptString += String.format("Total\t\t\t$%5.2f\n", this.getTotal() * (1.0 +TAX_RATE));
+        }
+        return receiptString;
     }
 
     public double getTotal() {
