d00788d81fb8e440dd01f1459e11ed93bf78ed6c
ebender17 <36086595+ebender17@users.noreply.github.com>
2019-10-23 13:38:28 -0400

    PizzaPlaceRunner

--- src/PizzaPlaceRunner.java
+++ src/PizzaPlaceRunner.java
@@ -1,5 +1,10 @@
 public class PizzaPlaceRunner {
     public static void main(String[] args) {
+        PizzaPlace pizzaPlace = new PizzaPlace("IT2045C", "123-123-1234");
+        PizzaGUI app = new PizzaGUI(pizzaPlace.getName(),
+                pizzaPlace.getCrusts(),
+                pizzaPlace.getSizes(),
+                pizzaPlace.getIngredients());
 
     }
 }
