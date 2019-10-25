f6d57da48ab9f315d91f9e14741f8ed99edc7cf8
ebender17 <36086595+ebender17@users.noreply.github.com>
2019-10-25 15:29:59 -0400

    KitchenView Frame

---
+++ src/KitchenView.java
@@ -0,0 +1,52 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+
+public class KitchenView extends JFrame implements Subscriber{
+    private JPanel mainPanel;
+    private JPanel orderPanel;
+    private JTextArea ordersTextArea;
+    private JScrollPane ordersScroll;
+    private JPanel quitPanel;
+    private JButton quitButton;
+
+
+    PizzaPlace pizzaPlace = PizzaPlace.getInstance("Kitchen View", "123-123-1234");
+
+    public KitchenView(String title) {
+        super(title);
+
+        add(mainPanel);
+        setVisible(true);
+        setSize(600,400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        quitButton.addActionListener((ActionEvent e) -> {
+            int selectedOption = JOptionPane.showConfirmDialog(
+                    null,
+                    "Are you sure you want to quit?",
+                    "Choose",
+                    JOptionPane.YES_NO_OPTION);
+            if (selectedOption == JOptionPane.YES_OPTION) {
+                System.exit(0);
+
+            }
+        });
+    }
+
+    private void updateOrdersArea() {
+        ArrayList<Order> allOrders = pizzaPlace.getOrders();
+        ordersTextArea.setText("");
+        for(Order order : allOrders) {
+            ordersTextArea.append(order.getView() + "n");
+
+
+        }
+
+    }
+    @Override
+    public void update() {
+        updateOrdersArea();
+
+    }
+}
