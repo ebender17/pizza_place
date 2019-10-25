383413ce48f2473856ba9ae7ecb1bc33929484be
ebender17 <36086595+ebender17@users.noreply.github.com>
2019-10-25 15:34:13 -0400

    finishing implementing singleton and observer in main method

--- src/PizzaPlaceRunner.java
+++ src/PizzaPlaceRunner.java
@@ -1,10 +1,13 @@
 public class PizzaPlaceRunner {
     public static void main(String[] args) {
-        PizzaPlace pizzaPlace = new PizzaPlace("IT2045C", "123-123-1234");
-        PizzaGUI app = new PizzaGUI(pizzaPlace.getName(),
-                pizzaPlace.getCrusts(),
-                pizzaPlace.getSizes(),
-                pizzaPlace.getIngredients());
+        PizzaPlace repo = PizzaPlace.getInstance("IT2045C", "123-123-1234");
+        PizzaGUI app = new PizzaGUI(repo.getName(),
+                repo.getCrusts(),
+                repo.getSizes(),
+                repo.getIngredients());
+        KitchenView kitchenView = new KitchenView("IT2045C Kitchen View");
+        repo.addSubscriber(kitchenView);
+
 
     }
 }
